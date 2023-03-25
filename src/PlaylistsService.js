20baf30de9919db27f5b8850857b93da5cf206cc
Afif <afifilhamcaniago@gmail.com>
2023-03-25 11:49:53 +0700

    done message broker

---
+++ src/PlaylistsService.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-underscore-dangle */
+const { Pool } = require('pg');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getPlaylistById(playlistId) {
+    const queryPlaylistInformation = {
+      text: 'SELECT id, name FROM playlists WHERE id = $1',
+      values: [playlistId],
+    };
+    const queryPlaylistSongs = {
+      text: `SELECT songs.id, songs.title, songs.performer FROM songs
+      INNER JOIN playlist_songs ON songs.id = playlist_songs.song_id
+      WHERE playlist_songs.playlist_id = $1`,
+      values: [playlistId],
+    };
+    const PlaylistInformation = await this._pool.query(queryPlaylistInformation);
+    const songs = await this._pool.query(queryPlaylistSongs);
+    return {
+      id: PlaylistInformation.rows[0].id,
+      name: PlaylistInformation.rows[0].name,
+      songs: songs.rows,
+    };
+  }
+}
+
+module.exports = PlaylistsService;
